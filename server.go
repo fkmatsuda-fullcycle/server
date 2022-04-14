4f76257bde01c467957fbd1b01ecb2f32067c0f9
Fábio Kenji Matsuda <fabio@fkmatsuda.dev>
2022-04-13 21:14:58 -0300

    feat: healthz

--- server.go
+++ server.go
@@ -4,9 +4,13 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 )
 
+var startedAt time.Time = time.Now()
+
 func main() {
+	http.HandleFunc("/healthz", healthz)
 	http.HandleFunc("/", hello)
 	http.ListenAndServe(":80", nil)
 }
@@ -16,5 +20,19 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	age := os.Getenv("AGE")
 
 	fmt.Fprintf(w, "Hello %s!!! You are %s years old.", name, age)
+
+}
+
+func healthz(w http.ResponseWriter, r *http.Request) {
+
+	duration := time.Since(startedAt)
+
+	if duration.Seconds() > 25 {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(fmt.Sprintf("Duration: %v", duration.Seconds())))
+	} else {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	}
 
 }
